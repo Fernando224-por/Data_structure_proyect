79190c4754fd1ad141e2ccd9b37c64a000563b2c
Fernando224-por <andresfernandoxd1591@gmail.com>
2025-05-10 20:40:51 -0500

    feat(SortData): add multi step data and Sort method for organize data

--- src/main/java/com/mycompany/graphwithhashmap/GraphWithHashMap.java
+++ src/main/java/com/mycompany/graphwithhashmap/GraphWithHashMap.java
@@ -1,52 +1,86 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.graphwithhashmap;
 
 /**
  *
  * @author andfe
  */
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Random;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GraphWithHashMap {
-    
+
     public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         simpleGraph g = new simpleGraph();
-        
+        simpleGraph g2 = new simpleGraph();
+        simpleGraph g3 = new simpleGraph();
+
         File myFile = new File("testFile.json");
-        
+
         if (myFile.exists()) {
+            Random r = new Random();
             JsonNode readJson = objectMapper.readTree(new File("testFile.json"));
-            JsonNode ubicaciones = readJson.get("Puntos");
-            for (JsonNode ubicacion : ubicaciones) {
+
+            JsonNode RouteOne = readJson.get("11_10_Bilbao");
+            System.out.println("Paradas de la ruta 11-10 Bilbao");
+            for (JsonNode ubicacion : RouteOne) {
                 String from = ubicacion.get("from").asText();
                 String to = ubicacion.get("to").asText();
                 Double weight = ubicacion.get("weight").asDouble();
-                
+
                 g.addEdges(from, to, weight);
-                
-                System.out.println("Los nodos " + from + " y " + to + " han sido añadidos");
-                
+
+                System.out.println("Los nodos " + from + " a " + to + " con el peso " + weight + " Han sido añadidos");
+
+            }
+            JsonNode RouteTwo = readJson.get("C_104_Suba_Compartir");
+            System.out.println("Paradas de la ruta C104 Suba Compartir");
+            for (JsonNode ubicacion : RouteTwo) {
+                String from = ubicacion.get("from").asText();
+                String to = ubicacion.get("to").asText();
+                Double weight = ubicacion.get("weight").asDouble();
+
+                g2.addEdges(from, to, weight);
+
+                System.out.println("Los nodos " + from + " a " + to + " con el peso " + weight + " Han sido añadidos");
+
+            }
+
+            JsonNode RouteThree = readJson.get("11_2_San_Andres");
+            System.out.println("Paradas de la ruta 11_2_San_Andres");
+            for (JsonNode ubicacion : RouteThree) {
+                String from = ubicacion.get("from").asText();
+                String to = ubicacion.get("to").asText();
+                Double weight = ubicacion.get("weight").asDouble();
+
+                g3.addEdges(from, to, weight);
+
+                System.out.println("Los nodos " + from + " a " + to + " con el peso " + weight + " Han sido añadidos");
+
             }
         } else {
             System.out.println("El archivo no existe");
         }
-        
-        simpleGraph.PathResult resultTestOne = g.findShortestPath("A", "G");
-        System.out.println(resultTestOne);
-        
-        simpleGraph.PathResult resultTestTwo = g.findShortestPath("B", "D");
-        System.out.println(resultTestTwo);
-        
-        simpleGraph.PathResult resultTestThree = g.findShortestPath("C", "F");
-        System.out.println(resultTestThree);
-        
+
+        simpleGraph.PathResult resultTestOne = g.findShortestPath("Portal_Suba", "Mi_Casa");
+
+        simpleGraph.PathResult resultTestTwo = g2.findShortestPath("Portal_Suba", "Mi_Casa");
+
+        simpleGraph.PathResult resultTestThree = g3.findShortestPath("Portal_Suba", "Mi_Casa");
+
+        ArrayList<simpleGraph.PathResult> paths = new ArrayList<>();
+
+        paths.add(resultTestOne);
+        paths.add(resultTestTwo);
+        paths.add(resultTestThree);
+
+        g.sortResult(paths);
     }
 }
--- src/main/java/com/mycompany/graphwithhashmap/simpleGraph.java
+++ src/main/java/com/mycompany/graphwithhashmap/simpleGraph.java
@@ -11,60 +11,64 @@ import java.util.PriorityQueue;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+
 /**
  *
  * @author andfe
  */
 public class simpleGraph {
+
     private Map<String, Map<String, Double>> graph = new HashMap<>();
+
     public void addNodes(String node) {
         graph.putIfAbsent(node, new HashMap<>());
     }
-    
-    public void addEdges (String from, String to, Double weight) {
+
+    public void addEdges(String from, String to, Double weight) {
         graph.putIfAbsent(to, new HashMap<>());
         graph.putIfAbsent(from, new HashMap<>());
-        
+
         graph.get(to).put(from, weight);
-        graph.get(from ).put(to, weight);
+        graph.get(from).put(to, weight);
     }
-    
-    public void printGraph(){
-        for (String node: graph.keySet()) {
-            System.out.println("El nodo "+ node + " esta unido a: " + graph.get(node));
+
+    public void printGraph() {
+        for (String node : graph.keySet()) {
+            System.out.println("El nodo " + node + " esta unido a: " + graph.get(node));
         }
     }
-    
+
     static class Node {
+
         String name;
         Double priority;
 
         Node(String name, Double priority) {
             this.name = name;
             this.priority = priority;
-        }        
+        }
     }
-    
-    public PathResult findShortestPath (String startNode, String finalNode) {
+
+    public PathResult findShortestPath(String startNode, String finalNode) {
         Map<String, Double> distance = new HashMap<>();
         Map<String, String> previus = new HashMap<>();
         PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingDouble(n -> n.priority));
-        
-        for (String node: graph.keySet()) {
+
+        for (String node : graph.keySet()) {
             if (node.equals(startNode)) {
                 distance.put(node, 0.0);
                 queue.add(new Node(node, 0.0));
             } else {
                 distance.put(node, Double.MAX_VALUE);
-                queue.add(new Node( node, Double.MAX_VALUE));
+                queue.add(new Node(node, Double.MAX_VALUE));
             }
             previus.put(node, null);
         }
-        
+
         while (!queue.isEmpty()) {
             Node current = queue.poll();
             String CurrentNode = current.name;
-            
+
             if (CurrentNode.equals(finalNode)) {
                 List<String> path = new ArrayList<>();
                 String step = finalNode;
@@ -74,12 +78,14 @@ public class simpleGraph {
                 }
                 return new PathResult(path, distance.get(finalNode));
             }
-            if (distance.get(CurrentNode) == Double.MAX_VALUE) break;
-            
-            for (Map.Entry<String, Double> neighbor : graph.get(CurrentNode). entrySet()) {
+            if (distance.get(CurrentNode) == Double.MAX_VALUE) {
+                break;
+            }
+
+            for (Map.Entry<String, Double> neighbor : graph.get(CurrentNode).entrySet()) {
                 String neighborNode = neighbor.getKey();
                 Double newDist = (Double) (distance.get(CurrentNode) + neighbor.getValue());
-                
+
                 if (newDist < distance.get(neighborNode)) {
                     distance.put(neighborNode, newDist);
                     previus.put(neighborNode, CurrentNode);
@@ -88,10 +94,19 @@ public class simpleGraph {
             }
         }
         return new PathResult(Collections.EMPTY_LIST, Double.MAX_VALUE);
-        
-    }    
-    
+
+    }
+
+    public void sortResult(ArrayList<simpleGraph.PathResult> resultados) {
+        Collections.sort(resultados, Comparator.comparingDouble(r -> r.distance));
+        System.out.println("organizar rutas de menor a mayor");
+        for (simpleGraph.PathResult resultado : resultados) {
+            System.out.println(resultado);
+        }
+    }
+
     static class PathResult {
+
         List<String> path;
         Double distance;
 
@@ -106,4 +121,3 @@ public class simpleGraph {
         }
     }
 }
-
